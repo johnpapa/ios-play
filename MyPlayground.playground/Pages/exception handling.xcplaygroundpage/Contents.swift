c116e28ef23e874184b88f41d15cc3c471f2dd6e
John Papa <john@johnpapa.net>
2016-08-15 15:26:22 -0400

    exception handling

---
+++ MyPlayground.playground/Pages/exception handling.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+
+
+func tryToWriteToFile(path: String, data: String) -> Bool {
+//  let path = "/usr/bin"
+  let encodedData = data.dataUsingEncoding(NSASCIIStringEncoding)
+  print(encodedData?.length)
+
+  do {
+    try encodedData?.writeToFile(path, options: [])
+
+    // Can't change encoding
+//    try data?.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding)
+    return true
+  }
+  catch let error as NSError {
+    print("Error saving file at path: \(path) with error: \(error)")
+  }
+  return false
+}
+
+tryToWriteToFile("/usr/bin/bar.txt", data: "Some data to write to a file")
+
+
+//// to get the docs dir
+func getDocumentsDirectory() -> NSString {
+  let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+  let documentsDirectory = paths[0]
+  return documentsDirectory
+}
+
+
