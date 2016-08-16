244d91d854ecc0e8dd3b8fa317a13f84d13deb67
John Papa <john@johnpapa.net>
2016-08-16 16:56:41 -0400

    autolayout with stackviews

---
+++ StackViewAutoLayout/StackViewAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewAutoLayout
+//
+//  Created by Papa, John L on 8/16/16.
+//  Copyright © 2016 Papa, John L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
