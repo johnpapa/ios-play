ebee42981d21b7e2dfcc09ff25d2c54a6fe6ab92
John Papa <john@johnpapa.net>
2016-08-19 11:20:46 -0400

    added an overlay activity indicator and an alert view controller

--- TopiTunesAppsDemo/TopiTunesAppsDemo/ViewController.swift
+++ TopiTunesAppsDemo/TopiTunesAppsDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   var apps: [ITunesApp] = []
   var limit: Int = 10 // default
   
-  @IBOutlet var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet var overlayView: UIView!
   @IBOutlet var tableView: UITableView!
   @IBOutlet var stepper: UIStepper!
   @IBOutlet var limitLabel: UILabel!
@@ -30,72 +30,110 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let url = NSURL(string: "https://itunes.apple.com/us/rss/topgrossingapplications/limit=\(limit)/json")!
     
     let session = NSURLSession.sharedSession()
- 
-    activityIndicator.startAnimating()
+    
+    self.overlayView.hidden = false
     let task = session.dataTaskWithURL(url) { (data, response, error) in
-      if let response = response {
-        print("Data encoding: \(response.textEncodingName)")
-      }
-      
-      if let error = error {
-        NSLog("Got an error!: \(error.localizedDescription)")
-        self.activityIndicator.stopAnimating()
-        return
-      }
-      
       if let data = data {
-        NSLog("Got \(data.length) bytes.")
-        let str = String(data: data, encoding: NSISOLatin1StringEncoding)
+        if let response = response {
+          print("Data encoding: \(response.textEncodingName)")
+        }
         
-        print("*** Here are the string contents:")
-        print(str)
-        print("*** ***")
+        if let error = error {
+          NSLog("Got an error!: \(error.localizedDescription)")
+          self.overlayView.hidden = true
+          return
+        }
         
-        let httpResponse = response as! NSHTTPURLResponse
-        let statusCode = httpResponse.statusCode
-        
-        if (statusCode == 200) {
-          print("Status code = \(statusCode)")
-          print("Everyone is fine, file downloaded successfully.")
+        do {
+          NSLog("Got \(data.length) bytes.")
+          let str = String(data: data, encoding: NSISOLatin1StringEncoding)
           
-          let possibleJson = self.parseJson(data)
-          if let json = possibleJson {
-            print(json["feed"])
-            print(json["feed"]!.dynamicType)
+          print("*** Here are the string contents:")
+          print(str)
+          print("*** ***")
+          
+          let httpResponse = response as! NSHTTPURLResponse
+          let statusCode = httpResponse.statusCode
+          
+          if (statusCode == 200) {
+            print("Status code = \(statusCode)")
+            print("Everyone is fine, file downloaded successfully.")
             
-            guard let dict = json["feed"] as? NSDictionary else { return }
-            guard let entries = dict["entry"] as? NSArray else { return }
-            
-            print(dict["entry"])
-            
-            for entry in entries {
-              guard let entryDict = entry as? NSDictionary else { continue }
-              guard let titleDict = entryDict["title"] as? NSDictionary else { continue }
-              guard let label = titleDict["label"] as? String else { continue }
+            let possibleJson = self.parseJson(data)
+            if let json = possibleJson {
+              print(json["feed"])
+              print(json["feed"]!.dynamicType)
               
-              print("*** label = \(label ?? " ")")
-
-              guard let summaryDict = entryDict["summary"] as? NSDictionary else { continue }
-              guard let summary = summaryDict["label"] as? String else { continue }
-
-              guard let images = entryDict["im:image"] as? NSArray else { continue }
-              let image = images[0] as! NSDictionary
-              let imageUrl = image["label"]! as! String
-              print("image url = \(imageUrl)")
+              guard let dict = json["feed"] as? NSDictionary else { return }
+              guard let entries = dict["entry"] as? NSArray else { return }
               
-              self.apps.append(ITunesApp(title: label, imageUrl: imageUrl, summary: summary))
-            }
-            
-            dispatch_async(dispatch_get_main_queue()) {
-              print(self.apps[0].title)
-              self.tableView.reloadData()
-              self.activityIndicator.stopAnimating() // TODO: where else do i stop?
+              print(dict["entry"])
+              
+              for entry in entries {
+                guard let entryDict = entry as? NSDictionary else { continue }
+                guard let titleDict = entryDict["title"] as? NSDictionary else { continue }
+                guard let label = titleDict["label"] as? String else { continue }
+                
+                print("*** label = \(label ?? " ")")
+                
+                guard let summaryDict = entryDict["summary"] as? NSDictionary else { continue }
+                guard let summary = summaryDict["label"] as? String else { continue }
+                
+                guard let images = entryDict["im:image"] as? NSArray else { continue }
+                let image = images[0] as! NSDictionary
+                let imageUrl = image["label"]! as! String
+                print("image url = \(imageUrl)")
+                
+                self.apps.append(ITunesApp(title: label, imageUrl: imageUrl, summary: summary))
+              }
+              
+              dispatch_async(dispatch_get_main_queue()) {
+                print(self.apps[0].title)
+                self.tableView.reloadData()
+                self.overlayView.hidden = true
+              }
             }
           }
+//        } catch (let error) {
+//          print("the big 'DO' caught an error")
+//          print(error)
+        }
+      }
+      else if let error = error {
+        let message = "unable to load the apps. \(error.localizedDescription)"
+        NSLog(message)
+        let alertVC = UIAlertController(title: "error loading",
+                                        message: message,
+                                        preferredStyle: .Alert)
+        let ok = UIAlertAction(title: "Ok", style: .Default, handler: { (action) in
+          NSLog("Ok pressed")
+        })
+        alertVC.addAction(ok)
+        self.presentViewController(alertVC, animated: true, completion: {
+          // do something if you want
+          // this happens when it is done presenting, not when they have hit OK
+        })
+        dispatch_async(dispatch_get_main_queue()) {
+          self.overlayView.hidden = true
         }
       }
     }
     task.resume()
+    
+//    else if let error = error {
+//      let message = "unable to load the apps. \(error.localizedDescription)"
+//      NSLog(message)
+//      let alertVC = UIAlertController(title: "error loading",
+//                                  message: message,
+//                                  preferredStyle: .Alert)
+//      let ok = UIAlertAction(title: "Ok", style: .Default, handler: { (action) in
+//        NSLog("Ok pressed")
+//      })
+//      self.presentViewController(alertVC, animated: true, completion: { 
+//        // do something if you want
+//        // this happens when it is done presenting, not when they have hit OK
+//      })
+//    }
   }
   
   func parseJson(data: NSData) -> [String: AnyObject]? {
@@ -118,6 +156,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.overlayView.hidden = true
     stepper.value = Double(limit)
     limitLabel.text = String(limit)
     tableView.estimatedRowHeight = 44.0
@@ -150,6 +189,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     appcell.appTitle?.text = apps[idx].title
     let url: String = (NSURL(string: apps[idx].imageUrl)?.absoluteString)!
     
+    // Get the image url
     NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: url)!, completionHandler: { (data, response, error) -> Void in
       if error != nil {
         print(error)
@@ -159,7 +199,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       dispatch_async(dispatch_get_main_queue(), { // () -> Void in
         let image = UIImage(data: data!)
         appcell.appImageView?.image = image
-//        appcell.layoutSubviews() // We don't need this here, but if we have rendering issues, we can
+        //        appcell.layoutSubviews() // We don't need this here, but if we have rendering issues, we can
       })
     }).resume()
     
